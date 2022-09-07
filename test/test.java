1912b788d9c68cd02513eb36edcbc3e1b1b81c51
x30021831 <xueyuqing@huawei.com>
2022-09-07 16:57:29 +0800

    divisible by 4

--- test/test.java
+++ test/test.java
@@ -1,10 +1,17 @@
 public class Test {
     public static void main(String[] args) {
-        int[] num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        int[] num = [0, 7, 52, 3, 4, 15, 6, 27, 8, 99];
         System.out.println(num[2]);
 
         String str = "null";
         int len = str.length();
         System.out.println(len);
+
+        int a = 4;
+        for (int i = 0; i < num.length; i++) {
+            int b = num[i];
+            int c = a / b;
+            print(c);
+        }
     }
 }
