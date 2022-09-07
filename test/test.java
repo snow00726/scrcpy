ed36ce9247713f3e0885ccad88e5047738367eec
x30021831 <xueyuqing@huawei.com>
2022-09-07 14:30:18 +0800

    fix:修复数组越界bug

--- test/test.java
+++ test/test.java
@@ -1,6 +1,9 @@
 public class Test {
     public static void main(String[] args) {
         int[] num = [];
+        for(int i = 0; i < 100; i++) {
+            num[i] = i;
+        }
         System.out.println(num[2]);
     }
 }
