f5ef3c9a612d00305dcb2e33984640b48658be07
x30021831 <xueyuqing@huawei.com>
2022-09-07 16:24:55 +0800

    print string length

--- test/test.java
+++ test/test.java
@@ -2,5 +2,9 @@ public class Test {
     public static void main(String[] args) {
         int[] num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
         System.out.println(num[2]);
+
+        String str = null;
+        int len = str.length();
+        System.out.println(len);
     }
 }
