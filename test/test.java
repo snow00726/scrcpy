0f8c99a630df7607c8906b516c9e2e73c599bf6e
x30021831 <xueyuqing@huawei.com>
2022-09-07 16:25:50 +0800

    fix NullPointer bug

--- test/test.java
+++ test/test.java
@@ -3,7 +3,7 @@ public class Test {
         int[] num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
         System.out.println(num[2]);
 
-        String str = null;
+        String str = "null";
         int len = str.length();
         System.out.println(len);
     }
